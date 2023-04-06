386c6387fa0c6aea727ec3260bfa2fbb46f7a60f
Nox04 <juan.angarita.11@gmail.com>
2023-04-06 14:40:42 -0500

    Method1

--- ingest.py
+++ ingest.py
@@ -5,32 +5,45 @@ import faiss
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 import pickle
-
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma
 
 # Here we load in the data in the format that Notion exports it in.
-ps = list(Path("Notion_DB/").glob("**/*.md"))
+ps = list(Path("Devbase_Coda/").glob("**/*.pdf"))
 
 data = []
 sources = []
-for p in ps:
-    with open(p) as f:
-        data.append(f.read())
-    sources.append(p)
 
-# Here we split the documents, as needed, into smaller chunks.
-# We do this due to the context limits of the LLMs.
-text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
-docs = []
-metadatas = []
-for i, d in enumerate(data):
-    splits = text_splitter.split_text(d)
-    docs.extend(splits)
-    metadatas.extend([{"source": sources[i]}] * len(splits))
+# Iterate through each PDF file
+for i, p in enumerate(ps):
+    # Load the PDF file using the PyPDFLoader
+    loader = PyPDFLoader(str(p))
+    pages = loader.load_and_split()
+    # Combine the text from all the pages and append it to the data list
+    data.extend(pages)
+
+    # Append the file path to the sources list
+    # sources.extend([{"source": sources[i]}] * len(p))
+
+print(len(data), len(sources))
+# # Here we split the documents, as needed, into smaller chunks.
+# # We do this due to the context limits of the LLMs.
+# docs = []
+# metadatas = []
+# for i, d in enumerate(data):
+#     docs.extend(d)
+#     metadatas.extend([{"source": sources[i]}] * len(d))
+# print(len(docs), len(metadatas))
+
+# # Here we create a vector store from the documents and save it to disk.
+# store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
+# faiss.write_index(store.index, "devbase.index")
+# store.index = None
+# with open("faiss_store_devbase.pkl", "wb") as f:
+#     pickle.dump(store, f)
 
 
-# Here we create a vector store from the documents and save it to disk.
-store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
-faiss.write_index(store.index, "docs.index")
-store.index = None
-with open("faiss_store.pkl", "wb") as f:
-    pickle.dump(store, f)
+embeddings = OpenAIEmbeddings()
+vectordb = Chroma.from_documents(data, embedding=embeddings,
+                                 persist_directory=".")
+vectordb.persist()
