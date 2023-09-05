80889676bf218d4595782736e237e26cbfba5148
Nox04 <juan.angarita.11@gmail.com>
2023-09-05 13:13:43 -0500

    Add everything

--- qa.py
+++ qa.py
@@ -1,5 +1,7 @@
 """Ask a question to the devbase asistant."""
 
+import random
+import string
 from langchain.document_loaders import PyPDFLoader  # for loading the pdf
 from langchain.embeddings import OpenAIEmbeddings  # for creating embeddings
 from langchain.vectorstores import Chroma  # for the vectorization part
@@ -9,19 +11,21 @@ from langchain.llms import OpenAI  # the LLM model we'll use (CHatGPT)
 from langchain.chat_models import ChatOpenAI
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from elevenlabs import generate, set_api_key
 
+set_api_key("7fa4f8ed6a63d186591151989dd4afe4")
 app = Flask("app")
 CORS(app)
 openai_api_key = "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"
-persist_directory = 'db_bluon'
-embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
-vectordb = Chroma(persist_directory=persist_directory,
-                  embedding_function=embedding)
-pdf_qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.9, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key),
-                                               vectordb.as_retriever(), return_source_documents=True)
 
 
-def process_input(user_input, chat_history):
+def process_input(user_input, chat_history, topic):
+    persist_directory = 'db_{}'.format(topic)
+    embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    vectordb = Chroma(persist_directory=persist_directory,
+                      embedding_function=embedding)
+    pdf_qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.9, model_name="gpt-4", openai_api_key=openai_api_key, max_tokens=2000),
+                                                   vectordb.as_retriever(), return_source_documents=True)
     result = pdf_qa({"question": user_input, "chat_history": chat_history})
     return result["answer"]
 
@@ -39,9 +43,22 @@ def query():
     for item in history:
         chat_history.append((item["message"], item["response"]))
 
-    response = process_input(data["message"], chat_history)
+    response = process_input(data["message"], chat_history, data["topic"])
+
+    audio = generate(
+        text=response,
+        voice="Bella",
+        model='eleven_monolingual_v1'
+    )
+
+    # generate a random filename
+    filename = ''.join(random.choices(
+        string.ascii_uppercase + string.digits, k=10))
+
+    with open('../malbec/public/'+filename+'.mp3', 'wb') as f:
+        f.write(audio)
 
     # Do something with the data here, for example:
-    processed_data = {"response": response}
+    processed_data = {"response": response, "audio": filename+'.mp3'}
 
     return jsonify(processed_data)
